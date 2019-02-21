ca907a7d930d2faf2ed738187c1359ad0da86d5a
xuziang111 <584788096@qq.com>
2019-02-21 18:53:15 +0800

    左侧css与js

--- JavaScript/main.js
+++ JavaScript/main.js
@@ -1,4 +1,20 @@
 (function(){
+    function leftresize(){
+        let wwidth = $(window).width()
+        if(wwidth-1200>0){
+            $(".tab-bar").css({'left':`${(wwidth-1200)/2}px`})
+        }else{
+            $(".tab-bar").css({'left':'0'})
+        }
+    }
+    $(window).resize(function() {
+        leftresize()
+      });
+      leftresize()
+$(window).scroll(function(){
+    $(".tab-bar").css({'left':`${-$(window).scrollLeft()}px`})
+})
+
 new Vue({
     // el:'#app',
 })
