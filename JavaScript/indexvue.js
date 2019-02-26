3c4585d01674f298e668b899c858690398f9fce4
xuziang111 <584788096@qq.com>
2019-02-26 14:32:15 +0800

    index ajax

--- JavaScript/indexvue.js
+++ JavaScript/indexvue.js
@@ -1,12 +1,12 @@
 let index = Vue.component('index',{
-    props:['indexdata'],
+    props:['indexdata','ifcreated'],
     template:`
     <div>
         <div class="left-side">
             <div>
                 <div class="block" id="top-carousel">
                     <el-carousel height="210px" arrow="never">
-                        <el-carousel-item v-for="(item,index) in indeximg">
+                        <el-carousel-item v-for="(item,index) in index.indeximg" :key="index">
                             <a :href="item.href"><img :src="item.img" alt="..."></a>
                             <h3>{{ item.abstract }}</h3>
                         </el-carousel-item>
@@ -16,7 +16,7 @@ let index = Vue.component('index',{
             
             <div class="partition-name"><h3>推荐文章</h3></div>
             <div class="article-content">
-                <section class="article" v-for="(item,index) in article">
+                <section class="article" v-for="(item,index) in index.article">
                 <a :href="item.href">
                 <div class="article-left">
                     <h4 class="article-tittle">xxx</h4>
@@ -28,17 +28,17 @@ let index = Vue.component('index',{
                 </section>               
             </div>
         </div>
-        <aside class="right-side">
-            <div>
-            <h4>热点</h4>
-            <hr>
-            <ul>
-                <li v-for="(item,index) in hot">
-                    <a :href="item.href">{{item.title}}</a><span>{{item.date}}</span>
-                </li>
-            </ul>
-            </div>
-        </aside>
+        <!--<aside class="right-side">-->
+            <!--<div>-->
+            <!--<h4>热点</h4>-->
+            <!--<hr>-->
+            <!--<ul>-->
+                <!--<li v-for="(item,index) in hot">-->
+                    <!--<a :href="item.href">{{item.title}}</a><span>{{item.date}}</span>-->
+                <!--</li>-->
+            <!--</ul>-->
+            <!--</div>-->
+        <!--</aside>-->
     </div>
     `,
     data:function(){
@@ -48,9 +48,7 @@ let index = Vue.component('index',{
                 {img:'./Images/8c007b5cgy1fqwefu5tkrj20xc0nkqv5.jpg',href:'#',abstract:'xxx'},
                 {img:'./Images/8c007b5cly1fkayrbqu9sj216b0ovke1.jpg',href:'#',abstract:"zzz"}
                 ],
-            article:[{title:'东京动画奖2019”年度最佳动画作品奖与个人奖结果发表',href:'#',img:'/Images/66474729_p0.jpg',abstract:'东京动画奖2019（TAAF2019）”的“年度最佳动画部门”作品奖与个人奖结果发表了。',date:'2019-2-21',source:'dmzj',type:'动画'},
-                {title:'《辉夜姬想让人告白》真人电影化决定！桥本环奈饰演辉夜',href:'#',img:'/Images/66474729_p0.jpg',abstract:'赤坂赤创作的漫画《辉夜姬想让人告白～天才们的恋爱头脑战～》将拍摄真人电影版，白银御行由平野紫耀出演，四宫辉夜由桥本环奈饰演。',date:'2019-2-21',source:'dmzj',type:'杂项'},
-            ],
+            article:[],
             hot:[{href:'#',title:'东京动画奖2019”年度最佳动画作品奖与个人奖结果发表',date:'2019-2-21'}]
         }
     },
@@ -74,10 +72,9 @@ let index = Vue.component('index',{
         }
     },
     created:function(){
-        this.$emit('loadindeximg',{page:'index',part:'indeximg'})
-        this.indexdata = index
-        // ajaxstart('indeximg')
-        // ajaxstart('indexarticle')
-        // ajaxstart('hot',function(data){data.hot = data})
+        if(this.ifcreated.index === 0){
+            this.$emit('loadindeximg',{page:'index',part:'indeximg'})
+            this.$emit('loadindearticle',{page:'index',part:'article'})
+        }
     }
 })
--- JavaScript/main.js
+++ JavaScript/main.js
@@ -36,9 +36,13 @@
         el:'#body-container',
         router,
         data:{
+                ifcreated:{index:0,},
                 hot:[{href:'#',title:'东京动画奖2019”年度最佳动画作品奖与个人奖结果发表',date:'2019-2-21'}],
                 index:{
-                        indeximg:[{img:'',href:''},],
+                        indeximg:[{img:'./Images/66474729_p0.jpg',href:'#',abstract:'ccc',},
+    {img:'./Images/8c007b5cgy1fqwefu5tkrj20xc0nkqv5.jpg',href:'#',abstract:'xxx'},
+    {img:'./Images/8c007b5cly1fkayrbqu9sj216b0ovke1.jpg',href:'#',abstract:"zzz"}
+                        ],
                         article:[{title:'',img:'',abstract:'',date:'',source:'',type:''}],
                         hot:[{href:'#',title:'loading...',date:'loading...'}]
                     },
@@ -54,17 +58,27 @@
                     type:'get',
                     url: `${e.page}/${e.part}`,
                     success: function(data){
-                        _temp.ajaxsuccess(data)
+                        this.e = e
+                        _temp.ajaxsuccess(data,e)
                     },
-                    error:function(){
-
+                    error:function(xxx){
+                        let data = [{title:'东京动画奖2019”年度最佳动画作品奖与个人奖结果发表',href:'#',img:'/Images/66474729_p0.jpg',abstract:'东京动画奖2019（TAAF2019）”的“年度最佳动画部门”作品奖与个人奖结果发表了。',date:'2019-2-21',source:'dmzj',type:'动画'},
+                            {title:'《辉夜姬想让人告白》真人电影化决定！桥本环奈饰演辉夜',href:'#',img:'/Images/66474729_p0.jpg',abstract:'赤坂赤创作的漫画《辉夜姬想让人告白～天才们的恋爱头脑战～》将拍摄真人电影版，白银御行由平野紫耀出演，四宫辉夜由桥本环奈饰演。',date:'2019-2-21',source:'dmzj',type:'杂项'},
+                        ]
+                        this.e = e
+                        console.log(e)
+                        _temp.ajaxsuccess(data,e)
                     },
                 });
             },
-            ajaxsuccess:function(data){
-                this[data.page][data.part] = data.data
+            ajaxsuccess:function(data,e){
+                this[e.page][e.part] = data
+                this.ifcreated[e.page]=1
             }
         },
+        created:function(){
+            this.ajaxstart({page:'index',part:'hot'})
+        }
     }).$mount('#body-container')
 
 })()
